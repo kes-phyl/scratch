c3146fbd1eff951e6c7d7feafebcf7d7eb0b3b42
kes-phyl <danphylbert@gmail.com>
2023-07-08 12:19:33 +0100

    implement decorators for route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from "@nestjs/common"
 
-@Controller()
+@Controller('/app')
 export class AppController{
-    @Get()
+    @Get('/asdf')
     getRootRoute(){
         return 'hi there!'
+    }
+    @Get('/bye')
+    getByeThere(){
+        return 'Bye there!'
     }
 }
